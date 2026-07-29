395fa3dd4b279965e9d0a670d3a61313638ff290
Nega <nerissagacheri@gmail.com>
2026-07-29 13:52:39 +0300

    Merge remote changes and restore stashed work

--- types/lucide-react.d.ts
+++ types/lucide-react.d.ts
@@ -17,6 +17,7 @@ declare module 'lucide-react' {
   export const ArrowUpRight: Icon;
   export const Award: Icon;
   export const BarChart3: Icon;
+<<<<<<< HEAD
   export const BookOpen: Icon;
   export const Building2: Icon;
   export const Check: Icon;
@@ -69,26 +70,67 @@ declare module 'lucide-react' {
   export const GraduationCap: Icon;
   export const Shield: Icon;
   export const HandHeart: Icon;
+=======
+>>>>>>> 1c0ffee (Merge remote changes and restore stashed work)
   export const BookOpen: Icon;
   export const Building2: Icon;
-  export const Sparkles: Icon;
-  export const BarChart3: Icon;
-  export const Award: Icon;
-  export const Trees: Icon;
-  export const Home: Icon;
-  export const Stethoscope: Icon;
-  export const Gift: Icon;
-  export const Smartphone: Icon;
-  export const Calendar: Icon;
-  export const HeartHandshake: Icon;
-  export const TrendingUp: Icon;
-  export const Search: Icon;
-  export const ShieldCheck: Icon;
-  export const PieChart: Icon;
-  export const FileText: Icon;
-  export const ChevronRight: Icon;
+  export const Check: Icon;
+  export const CheckCheck: Icon;
   export const CheckCircle: Icon;
+  export const CheckCircle2: Icon;
+  export const ChevronDown: Icon;
   export const ChevronLeft: Icon;
+  export const ChevronRight: Icon;
+  export const CircleCheckBig: Icon;
+  export const ClipboardList: Icon;
+  export const Clock: Icon;
+  export const Compass: Icon;
+  export const Droplets: Icon;
+  export const Eye: Icon;
+  export const FileText: Icon;
+  export const Gift: Icon;
+  export const Globe: Icon;
+  export const GraduationCap: Icon;
+  export const HandHeart: Icon;
+  export const Handshake: Icon;
+  export const Heart: Icon;
+  export const HeartHandshake: Icon;
   export const HeartPulse: Icon;
+<<<<<<< HEAD
 >>>>>>> 19d4b82e04745693f2a2394f9d5703c094902eef
+=======
+  export const Info: Icon;
+  export const Home: Icon;
+  export const Leaf: Icon;
+  export const Mail: Icon;
+  export const MapPin: Icon;
+  export const Maximize2: Icon;
+  export const Menu: Icon;
+  export const Mic: Icon;
+  export const Pause: Icon;
+  export const Phone: Icon;
+  export const PieChart: Icon;
+  export const Play: Icon;
+  export const Plus: Icon;
+  export const Quote: Icon;
+  export const Search: Icon;
+  export const Send: Icon;
+  export const SendHorizontal: Icon;
+  export const Shield: Icon;
+  export const ShieldCheck: Icon;
+  export const Smartphone: Icon;
+  export const Sparkles: Icon;
+  export const Star: Icon;
+  export const Stethoscope: Icon;
+  export const Target: Icon;
+  export const TrendingUp: Icon;
+  export const Trees: Icon;
+  export const UserCheck: Icon;
+  export const UserPlus: Icon;
+  export const Users: Icon;
+  export const Volume2: Icon;
+  export const VolumeX: Icon;
+  export const X: Icon;
+  export const Zap: Icon;
+>>>>>>> 1c0ffee (Merge remote changes and restore stashed work)
 }
