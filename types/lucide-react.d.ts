a4b6adb9827a46682d047974a262cb5eccd5961c
FeelhernRasheed <rafkarashid001@gmail.com>
2026-07-29 13:51:50 +0300

    about phase 1

--- types/lucide-react.d.ts
+++ types/lucide-react.d.ts
@@ -13,4 +13,20 @@ declare module 'lucide-react' {
   export const Menu: Icon;
   export const X: Icon;
   export const Heart: Icon;
+  export const Target: Icon;
+  export const Eye: Icon;
+  export const Users: Icon;
+  export const GraduationCap: Icon;
+  export const Shield: Icon;
+  export const Globe: Icon;
+  export const HandHeart: Icon;
+  export const BookOpen: Icon;
+  export const Building2: Icon;
+  export const ArrowRight: Icon;
+  export const Sparkles: Icon;
+  export const BarChart3: Icon;
+  export const Award: Icon;
+  export const Trees: Icon;
+  export const Home: Icon;
+  export const Stethoscope: Icon;
 }
