1d5475ce6306bf542008f3b56c919929c389c5c6
Nega <nerissagacheri@gmail.com>
2026-07-29 14:02:15 +0300

    Home page

--- types/lucide-react.d.ts
+++ types/lucide-react.d.ts
@@ -23,31 +23,17 @@ declare module 'lucide-react' {
   export const CheckCheck: Icon;
   export const CheckCircle: Icon;
   export const CheckCircle2: Icon;
-  export const CircleCheckBig: Icon;
   export const ChevronDown: Icon;
   export const ChevronRight: Icon;
+  export const CircleCheckBig: Icon;
   export const ClipboardList: Icon;
+  export const Clock: Icon;
   export const Compass: Icon;
   export const Droplets: Icon;
   export const Globe: Icon;
   export const HandHeart: Icon;
-  export const Heart: Icon;
-<<<<<<< Updated upstream
-  export const Phone: Icon;
-  export const Mail: Icon;
-  export const MapPin: Icon;
-  export const Clock: Icon;
-  export const Globe: Icon;
-  export const SendHorizontal: Icon;
-  export const ArrowRight: Icon;
-  export const Check: Icon;
-  export const Plus: Icon;
-  export const User: Icon;
-  export const MessageSquare: Icon;
   export const Handshake: Icon;
-  export const UserPlus: Icon;
-  export const Mic: Icon;
-=======
+  export const Heart: Icon;
   export const HeartHandshake: Icon;
   export const HeartPulse: Icon;
   export const Info: Icon;
@@ -56,20 +42,23 @@ declare module 'lucide-react' {
   export const MapPin: Icon;
   export const Maximize2: Icon;
   export const Menu: Icon;
+  export const Mic: Icon;
   export const Pause: Icon;
   export const Phone: Icon;
   export const Play: Icon;
+  export const Plus: Icon;
   export const Quote: Icon;
   export const Send: Icon;
+  export const SendHorizontal: Icon;
   export const ShieldCheck: Icon;
   export const Sparkles: Icon;
   export const Star: Icon;
   export const Target: Icon;
   export const UserCheck: Icon;
+  export const UserPlus: Icon;
   export const Users: Icon;
   export const Volume2: Icon;
   export const VolumeX: Icon;
   export const X: Icon;
   export const Zap: Icon;
->>>>>>> Stashed changes
 }
